251f397e9a1f2854e5454a67fc4711530db84db4
mumuy <>
2024-09-19 23:23:27 +0800

    updated 封装压缩

---
+++ rollup.config.mjs
@@ -0,0 +1,44 @@
+import resolve from '@rollup/plugin-node-resolve';          // 使用node_modules包
+import terser from '@rollup/plugin-terser';                 // 代码压缩
+import babel from '@rollup/plugin-babel';                   // ECMAScript兼容
+import pkg from './package.json' assert { type:'json' };    // 获取package信息
+
+// 版权信息
+const repository = pkg.repository.url.replace(/(.+)(:\/\/.+)\.git$/,'https$2');
+const now = new Date();
+const date = (new Date(now.getTime()-now.getTimezoneOffset()*60000)).toISOString().substring(0,10);
+const banner = `/*!
+ * ${pkg.name} v${pkg.version}
+ * ${pkg.description}
+ * ${pkg.homepage}
+ *
+ * Copyright (c) 2024-present, ${pkg.author}
+ *
+ * Released under the ${pkg.license} License
+ * ${repository}
+ *
+ * Created on: ${date}
+ */`;
+
+const commonPlugins = [
+    resolve(),
+    terser(),
+    babel({
+        babelHelpers: 'runtime',
+        exclude:'node_modules/**'
+    })
+];
+
+ export default [{
+    input: './src/gobang.js',
+    output:[{
+        file: pkg.main,
+        format: 'umd',
+        name: 'gobang',
+        banner
+    }],
+    plugins: commonPlugins,
+    watch: {
+        exclude: 'node_modules/**'
+    }
+}];
